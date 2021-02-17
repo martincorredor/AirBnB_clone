1f32cb4cd782f1405e563211afcb475a74dbb770
martincorredor <dev.martincorredor@gmail.com>
2021-02-17 11:11:55 -0500

    Add sorage functions

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.update_at = datetime.now()
-            storage.new()
+            storage.new(self)
 
     def __str__(self):
         """Prints class description"""
