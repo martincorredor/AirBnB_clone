f6b119132db2fb552d36fbccaa4fffa2f62fc475
martincorredor <dev.martincorredor@gmail.com>
2021-02-26 14:37:53 -0500

    Add base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,62 @@
 #!/usr/bin/python3
-"""Module for BaseModel"""
+"""Module for BaseModel class."""
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel():
-    """This is Base Class for this proyect"""
-
-    def __init__(self, *args, **kargs):
+    """BaseModel class."""
+    def __init__(self, *args, **kwargs):
         """
-        Constructor method
         Arguments
-            - *args: is a list with all individual arguments(won't be used)
-            - **kargs: is a dictionary with all arguments
-                    """
-        if kargs:
-            for k, v in kargs.items():
-                if "__class__" == k:
+          - *args: is a Tuple that contains all arguments (won’t be used).
+          - **kwargs: is a dictionary that contains all arguments by key/value.
+
+        Functions/methods:
+          - strptime(): creates a datetime object from the given instance.
+          - setattr(): sets the value of the attribute of an object.
+        """
+
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == '__class__':
                     pass
-                elif "created_at" == k:
-                    self.created_at = datetime.strptime(kargs["created_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif "updated_at" == k:
-                    self.updated_at = datetime.strptime(kargs["updated_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == 'created_at' or k == 'updated_at':
+                    setattr(self, k, datetime.
+                            strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
-        """Prints class description"""
-        return ("[{}] ({}) {}".
-                format(self.__class__.__name__, self.id, self.__dict__))
+        """
+        Used to find the human readable or "informal" storagering
+        representation of an object.
+        """
+        return "[{}] ({}) {}"\
+            .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
-        Updates the public instance atributte update_at
-        with the current datetime
+        Updates the public instorageance attribute updated_at with
+        the current datetime.
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
-        Return dictionary containing all keys/values
-        of __dict__ of the instance
+        Returns a dictionary containing all keys/values of __dict__
+        of the instorageance.
         """
-        my_dict = self.__dict__.copy()
-        my_dict.update({'__class__': self.__class__.__name__})
-        my_dict.update({'created_at': self.created_at.isoformat()})
-        my_dict.update({'updated_at': self.updated_at.isoformat()})
-        return my_dict
+        dictionary = self.__dict__.copy()
+        dictionary.update({'__class__': self.__class__.__name__})
+        dictionary.update({'created_at': self.created_at.isoformat()})
+        dictionary.update({'updated_at': self.updated_at.isoformat()})
+        return dictionary
+
